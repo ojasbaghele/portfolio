7d4b89aa1d304b58aec5da3229affd393459bbe7
Ojas Baghele <95533514+ojasbaghele@users.noreply.github.com>
2023-07-16 20:22:41 +0530

    /Portfolio
    PortFolio Website

---
+++ javascript.js
@@ -0,0 +1,10 @@
+//alert("Hello");
+// document.write(" Hellooo");
+
+
+
+
+var typed = new Typed('#element', {
+    strings: ['<h1>Web Developer</h1>', '<h1>Java Developer</h1>', '<h1>Full Stack Developer.</h1>'],
+    typeSpeed: 60,
+  });
