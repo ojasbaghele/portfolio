325ee4e0fdbbd8936450517bb2d31d6bed2e963c
Ojas Baghele <95533514+ojasbaghele@users.noreply.github.com>
2023-09-11 20:33:21 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -5,6 +5,6 @@
 
 
 var typed = new Typed('#element', {
-    strings: ['<h1>Web Developer</h1>', '<h1>Java Developer</h1>', '<h1>Full Stack Developer.</h1>'],
+    strings: ['<h1>Web Developer</h1>', '<h1>Java Developer</h1>', '<h1>PHP Developer</h1>', '<h1>Full Stack Developer.</h1>'],
     typeSpeed: 60,
   });
